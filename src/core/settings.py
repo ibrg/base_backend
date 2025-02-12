a211feea4dbba41f153e7726a9eb8299c6eaa58b
Roman <romanbezh@gmail.com>
2025-02-12 23:11:22 +0200

    fix docker config

--- src/core/settings.py
+++ src/core/settings.py
@@ -24,7 +24,7 @@ config = AutoConfig()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
